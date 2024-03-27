f8a20c17194c96889d52d307ec94790848661b66
syedfaisalmehmood <57040255+syedfaisalmehmood@users.noreply.github.com>
2024-03-27 13:39:04 +0500

    createed cli-number-guessing-game

---
+++ index.ts
@@ -0,0 +1,26 @@
+#! usr/bin/env node
+
+import inquirer from "inquirer";
+
+// 1) Computer will generate a random number - Done
+// 2) User input the guessing number - Done
+// 3) Compare the user input with Computer generated random number - Done
+// 4) show the results of number comparison - Done
+
+const randomnumber = Math.floor(Math.random() * 100 + 1 );  
+// console.log(randomnumber);  
+const answer = await inquirer.prompt([
+
+        {
+            name: "userGuessNumber",
+            type: "number",
+            message: "Please guess a Number between 1 - 100",
+        },
+])
+
+if(answer.userGuessNumber === randomnumber) {
+    console.log(`Congragulations! you guessed the right number. Becase computer generated number is ${randomnumber}`);
+    }
+else{console.log(`You guessed wrong number. Because computer generated number is ${randomnumber}`);
+}
+
